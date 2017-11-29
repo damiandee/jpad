afa52d7ffb4487aea1c46ef71a189828ab1f94b1
damiandee <damiandee02@gmail.com>
2017-11-29 13:28:57 +0100

    added normal distribution charts

--- Zadanie3/iris.py
+++ Zadanie3/iris.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import pylab as pl
 from scipy import stats
 
 with open('iris.data', 'r') as iris:
@@ -17,6 +18,24 @@ with open('iris.data', 'r') as iris:
 viriginica_array = np.array(viriginica_list)
 versicolor_array = np.array(versicolor_list)
 
+viriginica_array.sort()
+versicolor_array.sort()
+
+viriginica_fit = stats.norm.pdf(viriginica_array, np.mean(viriginica_array), np.std(viriginica_array))
+pl.plot(viriginica_array, viriginica_fit, '-o')
+pl.hist(viriginica_array, normed=True)
+pl.show()
+
+versicolor_fit = stats.norm.pdf(versicolor_array, np.mean(versicolor_array), np.std(versicolor_array))
+pl.plot(versicolor_array, versicolor_fit, '-o')
+pl.hist(versicolor_array, normed=True)
+pl.show()
+
 t, p = stats.ttest_ind(viriginica_array, versicolor_array)
 print("t = " + str(t))
 print("p = " + str(p))
+
+if p < 0.05:
+    print("\nKwiaty Iris Viriginica oraz Iris Versicolor można rozróżnić na podstawie atrybutu 'sepal width'")
+else:
+    ("Kwiatów nie da się rozróżnić na podstawie atrybutu 'sepal width'")
